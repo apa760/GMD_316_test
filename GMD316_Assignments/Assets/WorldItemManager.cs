57ca9b618d85ba7e8ae30a19bfd7dfa42e3aeeb3
apa760 <90884162+apa760@users.noreply.github.com>
2026-09-13 18:20:46 -0400

    week 2 assignment
    code requried for assignment

--- GMD316_Assignments/Assets/WorldItemManager.cs
+++ GMD316_Assignments/Assets/WorldItemManager.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
+using System;
+using System.IO;
+using System.Linq;
 
 public class WorldItemManager : MonoBehaviour
 {
 
-    public SOItem[] ItemsAll;
+    //public SOItem[] ItemsAll;
+    public SOItem[] spawnedItems;
 
     public GameObject itemObject;
+
+    public string allItemsFilePath;
+    public SOItem newItemSO;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        
+    {   
+        allItemsFilePath = Path.Combine(Application.dataPath, "SOItem.json");
+        newItemSO = ScriptableObject.CreateInstance<SOItem>();
     }
 
     // Update is called once per frame
@@ -20,19 +28,25 @@ public class WorldItemManager : MonoBehaviour
 
         if(Input.GetMouseButtonUp(1))//Right Mouse button
         {
-            int randNum = Random.Range(0, ItemsAll.Length - 1);
-            SOItem newItemSelected = ItemsAll[randNum];
+            newItemSO.itemName = "Bush Seed";
+            //pull info from json to create a new item SO
 
-            GameObject newItemObject = Instantiate(itemObject);
-            Vector3 mousePos = Input.mousePosition;
-            mousePos.z = Camera.main.nearClipPlane;
-            Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
-            Vector2 worldPos2D = new Vector2(worldPos.x, worldPos.y);
-            newItemObject.transform.position = worldPos2D;
-            ItemToPickUp itemScript = newItemObject.GetComponent<ItemToPickUp>();
-            itemScript.itemSO = newItemSelected;
-            newItemObject.GetComponent<SpriteRenderer>().sprite = newItemSelected.icon;
-            newItemObject.GetComponent<SpriteRenderer>().color = newItemSelected.color;
+            spawnedItems.Append(newItemSO); // ITS NOT APPENDING
+
+
+            // int randNum = Random.Range(0, ItemsAll.Length - 1);
+            // SOItem newItemSelected = ItemsAll[randNum];
+
+            // GameObject newItemObject = Instantiate(itemObject);
+            // Vector3 mousePos = Input.mousePosition;
+            // mousePos.z = Camera.main.nearClipPlane;
+            // Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+            // Vector2 worldPos2D = new Vector2(worldPos.x, worldPos.y);
+            // newItemObject.transform.position = worldPos2D;
+            // ItemToPickUp itemScript = newItemObject.GetComponent<ItemToPickUp>();
+            // itemScript.itemSO = newItemSelected;
+            // newItemObject.GetComponent<SpriteRenderer>().sprite = newItemSelected.icon;
+            // newItemObject.GetComponent<SpriteRenderer>().color = newItemSelected.color;
 
         }
     }
